8ca643162fada46ca4662c2f536dc2f2139f7a25
Ostap95 <ostap_95@hotmail.com>
2017-05-25 03:11:47 -0700

    ssh and vpn improvements

--- sshTunnel.py
+++ sshTunnel.py
@@ -1,6 +1,10 @@
 import subprocess
-tunnelToFTP = "ssh -f -L 7080:192.168.0.3:22 superuser@10.0.0.131 -N"
-tunnelToManager = "ssh -f -L 8080:192.168.0.2:22 superuser@10.0.0.131 -N"
+tunnelToFTP_DNS1 = "ssh -f -L 7080:192.168.0.3:22 superuser@10.0.0.131 -N"
+tunnelToManager_DNS1 = "ssh -f -L 7085:192.168.0.2:22 superuser@10.0.0.131 -N"
+tunnelToFTP_DNS2 = "ssh -f -L 7090:192.168.0.3:22 superuser@10.0.0.130 -N"
+tunnelToManager_DNS2 = "ssh -f -L 7095:192.168.0.2:22 superuser@10.0.0.130 -N"
+tunnelToFTP_Proxy = "ssh -f -L 7100:192.168.0.3:22 superuser@10.0.0.132 -N"
+tunnelToManager_Proxy = "ssh -f -L 7105:192.168.0.2:22 superuser@10.0.0.132 -N"
 
 def run_script(script, stdin=None):
 	try:
@@ -11,8 +15,21 @@ def run_script(script, stdin=None):
 	except Exception:
 		print "Couldn't create tunnel"
 
-print "#####|Creating tunnel to FTP Server on port 7080|#####"
-run_script(tunnelToFTP)
+print "#####|Creating tunnel to FTP Server on port 7080 with Primary DNS Server|#####"
+run_script(tunnelToFTP_DNS1)
 
-print "#####|Creating tunnel to Manager Server on port 8080|#####"
-run_script(tunnelToManager)
+print "#####|Creating tunnel to Manager Server on port 7085 with Primary DNS Server|#####"
+run_script(tunnelToManager_DNS1)
+
+print "#####|Creating tunnel to FTP Server on port 7090 with Secondary DNS Server|#####"
+run_script(tunnelToFTP_DNS2)
+
+print "#####|Creating tunnel to Manager Server on port 7095 with Secondary DNS Server|#####"
+run_script(tunnelToManager_DNS2)
+
+print "#####|Creating tunnel to FTP Server on port 7100 with Proxy Server|#####"
+run_script(tunnelToFTP_Proxy)
+
+print "#####|Creating tunnel to Manager Server on port 7105 with Proxy Server|#####"
+run_script(tunnelToManager_Proxy)
+
